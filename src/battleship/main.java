1598deb94695a35aad6a07ed30a8708c733a8a1c
Taras Boreyko <tarasyo7@gmail.com>
2018-03-21 18:43:27 +0000

    Create main and ask user how many players

---
+++ src/battleship/main.java
@@ -0,0 +1,11 @@
+package battleship;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		player play = new player();
+	}
+
+}
--- src/battleship/player.java
+++ src/battleship/player.java
@@ -1,5 +1,40 @@
 package battleship;
 
+import java.util.Scanner;
+
 public class player {
+	
+	//Creation of global arrays where all values will be writen 
+	private String[] name;
+	private int[] age;
+	private String[] email;
+	private int[] points;
+	
+	public player() {
+		
+		System.out.println("Welcome");
+		System.out.println("Chuse hove many players will play");
+		System.out.println("MIN=1; MAX=4;");
+		System.out.println("Write plese number");
+		
+		boolean valid = true;
+		//Loop for asking input until user entered number from 1 to 4
+		do {
+		
+		// Reading input
+		Scanner in = new Scanner(System.in);
+		String nPlayers = in.nextLine();
+		
+		//if number in nPlayers mathces from 1 to 4 everithing is goood
+		if(nPlayers.matches("[1-4]")) {
+			valid=true;
+		}else {
+			valid=false;
+			System.out.println("*****ERROR*****");
+			System.out.println("Please Enter number from 1 to 4");
+		}
+		
+		}while(valid==false);
+	}
 
 }
