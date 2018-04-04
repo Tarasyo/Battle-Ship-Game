ad7ba18258cd7d5b19de8db0a7a10b0e45507171
Taras Boreyko <tarasyo7@gmail.com>
2018-04-04 16:06:36 +0100

    Finished the printing board and Intilialisation of size

--- src/battleship/board.java
+++ src/battleship/board.java
@@ -6,10 +6,12 @@ import java.util.Scanner;
 //TARAS BOREYKO 
 //ID 2017284
 public class board {
-	private int hight;
-	private int width;
+	//I decide to write columns and rows not width and height
+	private int row;
+	private int col;
 	//This array for board
-	private String Board[][] = new String[21][21];
+	private String Board[][];
+	
 	
 	public board() {
 		
@@ -30,12 +32,12 @@ public class board {
 		
 		//check if input is numbers 
 		if(width.matches("[0-9]+") && hight.matches("[0-9]+")) {
-			
-			this.width = Integer.valueOf(width);
-			this.hight = Integer.valueOf(hight);
+		// Initialising rows and colum after check if input is numbers 
+			this.col = Integer.valueOf(width);
+			this.row = Integer.valueOf(hight);
 			
 			//check if size will be from 10 to 20;
-			if((this.width>=10 && this.hight>=10) && (this.width<=20 && this.hight<=20)) {
+			if((this.col>=10 && this.row>=10) && (this.col<=20 && this.row<=20)) {
 			valid = true;
 			}else { 
 			valid = false;
@@ -48,21 +50,25 @@ public class board {
 		}
 		}while(valid==false);
 	   
-		System.out.println(width+ " " + hight);
+		System.out.println(col+ " " + row);
+	}
+	//Initialisation for size of Array and bring it to main
+	public String[][] getBoard() {
+			return Board = new String[row+1][col+1];
 	}
 	
 	//Setting first board that will be showing on  the beginning of the game
 	public void firstBoard() {
 		int i,j;
 		//in the coordinate 0 0 will be empty 
-		Board[0][0] = " ";
-		// this loop to write number coordinates on  0 row from 1 to size of width 
-		for(j=1; j<11; j++) {
+		Board[0][0] = "  ";
+		// this loop to write number coordinates on  0 row from 1 to 9 
+		for(j=1; j<10; j++) {
 			Board[0][j] = "  "+String.valueOf(j);
 		}
 		
 		//loop to write numbers in coloms after 10, that board looks nice 
-		for(j=11; j<=width; j++)
+		for(j=10; j<=col; j++)
 			Board[0][j] = " "+String.valueOf(j); 
 				
 		// this loop to write numbers of coordinates on 0 column from 1 to 9
@@ -71,23 +77,23 @@ public class board {
 		}
 		
 		//loop to write numbers in row after 10, that board looks nice 
-		for(i=10; i<=hight; i++)
+		for(i=10; i<=row; i++)
 		Board[i][0] = String.valueOf(i)+"|"; 
 		
 		//this double loop to write actual board 
-		for(i=1; i<=hight; i++) {
-			for(j=1; j<=width; j++) {
+		for(i=1; i<=row; i++) {
+			for(j=1; j<=col; j++) {
 				Board[i][j] = "_ |";
 			}
 		}
 	}
-	//method to print board, I can call this method when I like and print new boar after chenges 
+	//method to print board, I can call this method when I need to print new boar after chenges 
 	public void printBoard() {
 		int i,j;
 		
-		for(i=0; i<=hight; i++) {
+		for(i=0; i<=row; i++) {
 			
-			for(j=0; j<=width; j++) {
+			for(j=0; j<=col; j++) {
 				System.out.print(Board[i][j]);
 			}
 			System.out.println("");//start new row
--- src/battleship/main.java
+++ src/battleship/main.java
@@ -6,7 +6,11 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		board b = new board();
-	
+		b.Size();
+		b.getBoard();
+		b.firstBoard();
+		b.printBoard();
+		
 
 		
 	}
